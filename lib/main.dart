d66f3c9e4b65c68d81d8f5a1b89571797e33ac31
ranoa <ranoaritsiky@gmail.com>
2022-02-02 09:17:10 +0300

    learn layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Padding(
+      padding: EdgeInsets.all(50.0),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.green,
+            border: Border.all(
+                color: Colors.red,
+                width: 3.0,
+                style: BorderStyle.solid
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 7,
+                  offset: Offset(4,8),
+                  spreadRadius: 2.00
+
+              )
+            ]
+        ),
+        width: 150.0,
+        height: 300.0,
+        child: Text(
+          "hello my friend",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 15.0,
+            fontFamily: 'lato',
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
